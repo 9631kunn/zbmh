c6a0e0cc33c037e36a3d913952ff3b76d2bcc088
9631kunn <infourbh+1@gmail.com>
2020-09-06 11:40:39 +0900

    fix: pages

--- src/pages/404.js
+++ src/pages/404.js
@@ -3,9 +3,9 @@ import React from "react"
 import Layout from "../components/Layout"
 import SEO from "../components/Seo"
 
-const NotFoundPage = () => (
+const NotFoundPage = props => (
   <Layout>
-    <SEO title="404: Not found" />
+    <SEO title="404: Not found" location={props.location} />
     <h1>NOT FOUND</h1>
     <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
   </Layout>
